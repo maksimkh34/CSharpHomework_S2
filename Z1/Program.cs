1b4fa4c3d92351a9499861470f38188f9f30fd72
Maksim <epic.twod@mail.ru>
2022-06-10 12:30:13 +0300

    Task 1

---
+++ Z1/Program.cs
@@ -0,0 +1,19 @@
+﻿int showsecdigit(int a)
+{
+    if(a>99 && a<1000)
+    {
+        return (a/10)%10;
+    }
+    else return -1;
+}
+
+Console.Write("Enter your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = showsecdigit(a);
+if(result==-1)
+{
+    Console.WriteLine("Error");
+}
+else{
+    Console.WriteLine("Middle digit is: "+result);
+}
