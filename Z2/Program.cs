d41ddcce4cafce4f55af601b82869f7d1b9602e1
Maksim <epic.twod@mail.ru>
2022-06-11 11:40:11 +0300

    Task 2

---
+++ Z2/Program.cs
@@ -0,0 +1,37 @@
+﻿int showthrddigit(int x)
+{
+    if(x<100 ^ x<-100)
+    {
+        return -1;
+    }
+    else 
+    {
+        if(x>=0) 
+        {
+            while(x>999)
+            {
+                x = x/10;
+            }
+                return x%10;
+        }
+        else 
+        {
+            while(x<-999)
+            {
+                x = x/10;
+            }
+            return -1*(x%10);
+        }
+    }
+}
+
+Console.Write("Enter your number: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+int result = showthrddigit(a);
+if(result==-1)
+{
+    Console.WriteLine("Error");
+}
+else{
+    Console.WriteLine("Third digit is: "+result);
+}
