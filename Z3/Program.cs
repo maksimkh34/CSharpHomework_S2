21776c8d2eb837d5824479b405af7311831a2022
Maksim <epic.twod@mail.ru>
2022-06-11 11:38:26 +0300

    All tasks done!

---
+++ Z3/Program.cs
@@ -0,0 +1,31 @@
+﻿int isoffday(int a)
+{
+    if(a<=5 && a>0)
+    {
+       return 0;
+    }
+    else if(a==6 || a==7)
+    {
+        return 1;
+    }
+    else return -1;
+}
+
+
+
+Console.Write("Enter your number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int day = isoffday(a);
+Console.Write("This day is");
+if(day == 0)
+{
+    Console.Write(" weekday. ");
+}
+else if(day == 1)
+{
+    Console.Write(" off day! :)");
+}
+else
+{
+    Console.Write("... Something went wrong. Enter number again.");
+}
